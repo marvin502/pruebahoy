17b723aec5e5670e633061b977774528d2be4674
marvin502 <marvinarnoldojime@gamil.com>
2022-07-28 14:38:58 -0600

    hola

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio de Prueba</title>
+</head>
+<body>
+    <h1>HOLA MUNDO</h1>
+    <h2>HOLA MUNDO</h2>
+    <h3>HOLA MUNDO</h3>
+    <h4>HOLA MUNDO</h4>
+    <h5>HOLA MUNDO</h5>
+    <h6>HOLA MUNDO</h6>
+</body>
+</html>
